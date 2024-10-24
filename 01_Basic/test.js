12a14c195c2c349330539d82086a2293a38ca4cf
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-24 10:00:53 +0000

    Update

--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -2,3 +2,8 @@ console.log("YUSUF");
 
 const name = "Yusuf Shaikh Update 23-10-2024";
 console.log(name)
+
+const firstName = "Yusuf";
+const lastName = "Shaikh";
+
+console.log(`this is my first name ${firstName} and last name ${lastName}`)
